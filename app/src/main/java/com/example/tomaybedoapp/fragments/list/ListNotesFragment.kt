6705e445e611c39c458d7184a0321a4531031749
LeoCatarro <leonardocatarro@gmail.com>
2021-05-26 16:29:54 +0100

    Delete All Notes (DONE!)

--- app/src/main/java/com/example/tomaybedoapp/fragments/list/ListNotesFragment.kt
+++ app/src/main/java/com/example/tomaybedoapp/fragments/list/ListNotesFragment.kt
@@ -1,10 +1,10 @@
 package com.example.tomaybedoapp.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -42,6 +42,35 @@ class ListNotesFragment : Fragment() {
             findNavController().navigate(R.id.action_listNotes_to_addNotes)
         }
 
+        //Add delete menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.delete_note_menu){
+            deleteAllNotes()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllNotes() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") {_, _ ->
+            //If pressed "Yes" lets remove the Note from DB!
+            mNoteViewModel.deleteAllNotes()
+            //Successfuly removed note message
+            Toast.makeText(requireContext(), "All Notes Successfully Removed!", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") {_, _ -> }
+        builder.setTitle("Delete All?")
+        builder.setMessage("Are you about to delete all notes?")
+        builder.create().show()
     }
 }

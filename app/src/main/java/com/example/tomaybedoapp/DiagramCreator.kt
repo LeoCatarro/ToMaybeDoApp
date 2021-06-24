96277b6ad42ff7862ae56a4ca9a49743b9fb1515
LeoCatarro <leonardocatarro@gmail.com>
2021-06-24 19:17:43 +0100

    Diagram Creator:     - Create Paths     - Undo && Redo

--- app/src/main/java/com/example/tomaybedoapp/DiagramCreator.kt
+++ app/src/main/java/com/example/tomaybedoapp/DiagramCreator.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_diagram_creator.*
 
 class DiagramCreator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,18 @@ class DiagramCreator : AppCompatActivity() {
         title="ToMaybeDoApp";
 
         //Main Buttons OnClickListener events
-        val button: Button = findViewById(R.id.btnOpenMain);
+        /*val button: Button = findViewById(R.id.btnOpenMain);
         button.setOnClickListener {
             val intent = Intent(this@DiagramCreator, MainActivity::class.java)
             startActivity(intent)
+        }*/
+
+        ControlUndo.setOnClickListener {
+            PaintArea.setUndo()
+        }
+
+        ControlRedo.setOnClickListener {
+            PaintArea.setRedo()
         }
     }
 }

adaa67e99db83b18ba84b45347b0b3482536a469
LeoCatarro <leonardocatarro@gmail.com>
2021-06-30 23:25:01 +0100

    Added back button in DiagramCreator.kt (without function)

--- app/src/main/java/com/example/tomaybedoapp/DiagramCreator.kt
+++ app/src/main/java/com/example/tomaybedoapp/DiagramCreator.kt
@@ -1,17 +1,17 @@
 package com.example.tomaybedoapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_diagram_creator.*
+
 
 class DiagramCreator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagram_creator)
 
-        title="ToMaybeDoApp";
+        title="ToMaybeDoApp22";
 
         ControlUndo.setOnClickListener {
             PaintArea.setUndo()
@@ -24,5 +24,7 @@ class DiagramCreator : AppCompatActivity() {
         ClearAllPaths.setOnClickListener {
             PaintArea.setDeleteAll()
         }
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);     //Back button to return to main activity
     }
 }
